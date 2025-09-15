a92297bdc752c40c91223468a7dc427213a1197a
Yogesh Rana <ranayogesh794@gmail.com>
2025-09-16 02:05:23 +0530

    Refactor CORS configuration to use a constant for frontend URL

--- Backend/index.js
+++ Backend/index.js
@@ -13,21 +13,23 @@ import userRouter from "./routes/user.routes.js";
 dotenv.config();
 const app = express();
 let server = http.createServer(app);
+const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
+
+app.use(
+    cors({
+        origin: FRONTEND_URL,
+        credentials: true,
+    })
+);
+
 export const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: FRONTEND_URL,
         credentials: true,
     },
 });
 
 let port = process.env.PORT || 5000;
-
-app.use(
-    cors({
-        origin: "http://localhost:5173",
-        credentials: true,
-    })
-);
 
 app.use(express.json());
 app.use(cookieParser());
