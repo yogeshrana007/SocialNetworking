acf14224509c8e7dc3c44a660630666301e9b4e8
Yogesh Rana <ranayogesh794@gmail.com>
2025-09-17 15:18:09 +0530

    refactor: enhance search functionality in Navbar component

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,9 +1,10 @@
 import axios from "axios";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { FaUserGroup } from "react-icons/fa6";
 import { IoNotificationsSharp, IoSearchSharp } from "react-icons/io5";
+import { MdCancel } from "react-icons/md";
 import { TiHome } from "react-icons/ti";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import DP from "../assets/DP.png";
 import logo2 from "../assets/logo2.png";
 import { AuthDataContext } from "../context/AuthContext.jsx";
@@ -11,8 +12,14 @@ import { UserDataContext } from "../context/UserContext.jsx";
 
 function Navbar() {
     let [activeSearch, setActiveSearch] = useState(false);
-    let { userData, setUserData, showPopup, setShowPopup } =
-        useContext(UserDataContext);
+    let {
+        userData,
+        setUserData,
+        showPopup,
+        setShowPopup,
+        showSearch,
+        setShowSearch,
+    } = useContext(UserDataContext);
     let { serverUrl } = useContext(AuthDataContext);
     let navigate = useNavigate();
 
@@ -31,6 +38,30 @@ function Navbar() {
         }
     };
 
+    let [searchInput, setSearchInput] = useState("");
+    let [searchedData, setSearchedData] = useState([]);
+
+    const handleSearch = async () => {
+        try {
+            let result = await axios.get(
+                `${serverUrl}/api/user/search?query=${searchInput}`,
+                { withCredentials: true }
+            );
+            setSearchedData(result.data);
+            console.log(result.data);
+        } catch (error) {
+            console.log("search err", error);
+        }
+    };
+
+    useEffect(() => {
+        if (searchInput) {
+            handleSearch();
+            setShowSearch(true);
+        } else {
+            setShowSearch(false);
+        }
+    }, [searchInput]);
     return (
         <>
             <div className="w-full h-[10vh] bg-[hsl(0,0%,100%)] shadow-md flex justify-between md:justify-around items-center px-[10px] z-[80] fixed">
@@ -40,6 +71,7 @@ function Navbar() {
                         onClick={() => {
                             setShowPopup(false);
                             setActiveSearch(false);
+                            setShowSearch(false);
                         }}
                         className="cursor-pointer"
                     >
@@ -61,22 +93,83 @@ function Navbar() {
                             />
                         </div>
                     )}
-                    <form
-                        className={`w-[200px] lg:w-[350px] h-[40px] bg-[#f0efe7] lg:flex items-center gap-[10px] px-[10px] py-[5px] rounded-md ${
-                            !activeSearch ? "hidden" : "flex"
-                        }`}
-                    >
-                        <div className="">
-                            <IoSearchSharp className="w-[22px] h-[22px] text-gray-700" />
+                    <div className="">
+                        <div>
+                            <form
+                                className={`w-[200px] lg:w-[350px] h-[40px] bg-[#f0efe7] lg:flex items-center gap-[10px] px-[10px] py-[5px] rounded-md ${
+                                    !activeSearch ? "hidden" : "flex"
+                                }`}
+                            >
+                                <div className="">
+                                    <IoSearchSharp className="w-[22px] h-[22px] text-gray-700" />
+                                </div>
+                                <input
+                                    type="text"
+                                    className="w-[80%] h-full bg-transparent outline-none bottom-0"
+                                    placeholder="search users.."
+                                    onChange={(e) =>
+                                        setSearchInput(e.target.value)
+                                    }
+                                    value={searchInput}
+                                />
+                                {searchInput != "" ? (
+                                    <MdCancel
+                                        className="cursor-pointer"
+                                        onClick={() => setSearchInput("")}
+                                    />
+                                ) : (
+                                    ""
+                                )}
+                            </form>
                         </div>
-                        <input
-                            type="text"
-                            className="w-[80%] h-full bg-transparent outline-none bottom-0"
-                            placeholder="search users.."
-                        />
-                    </form>
+                        <div className="absolute">
+                            {/* showing searched users */}
+                            {showSearch && (
+                                <div className="w-[300px] mt-[20px] bg-white">
+                                    {searchedData.length === 0 ? (
+                                        <p className="p-4 text-gray-500">
+                                            No user found
+                                        </p>
+                                    ) : (
+                                        searchedData.map((user) => (
+                                            <div
+                                                key={user._id}
+                                                className="flex items-center justify-between p-2 border-b bg-white px-[20px] md:px-[40px]"
+                                            >
+                                                <Link
+                                                    to={`/profile/${user._id}`}
+                                                >
+                                                    <div className="flex items-center gap-2">
+                                                        <img
+                                                            src={
+                                                                user
+                                                                    ?.profileImage
+                                                                    ?.url || DP
+                                                            }
+                                                            alt="profile"
+                                                            className="w-10 h-10 rounded-full object-cover"
+                                                        />
+                                                        <div>
+                                                            <p className="font-semibold">
+                                                                {
+                                                                    user?.firstName
+                                                                }{" "}
+                                                                {user?.lastName}
+                                                            </p>
+                                                            <p className="text-sm text-gray-600">
+                                                                {user?.headline}
+                                                            </p>
+                                                        </div>
+                                                    </div>
+                                                </Link>
+                                            </div>
+                                        ))
+                                    )}
+                                </div>
+                            )}
+                        </div>
+                    </div>
                 </div>
-
                 {/* Right div */}
                 <div className="flex justify-center items-center gap-[20px] ">
                     {/* Profile drawer */}
@@ -100,6 +193,7 @@ function Navbar() {
                             <button
                                 className="w-[85%] h-[40px] border-2 border-[#2dc0ff] text-[#2dc0ff] rounded-full"
                                 onClick={() => {
+                                    setShowSearch(false);
                                     setShowPopup(false);
                                     navigate("/profile");
                                 }}
@@ -114,6 +208,7 @@ function Navbar() {
                             <div
                                 className=" w-full flex items-center justify-start text-gray-600 hover:text-gray-900 pl-[20px] gap-[10px] cursor-pointer"
                                 onClick={() => {
+                                    setShowSearch(false);
                                     setShowPopup(false);
                                     navigate("/network");
                                 }}
@@ -135,6 +230,7 @@ function Navbar() {
                     <div
                         className=" lg:flex flex-col items-center justify-center text-[14px] text-gray-600 hover:text-gray-900 hidden cursor-pointer"
                         onClick={() => {
+                            setShowSearch(false);
                             setShowPopup(false);
                             navigate("/");
                         }}
@@ -147,6 +243,7 @@ function Navbar() {
                     <div
                         className=" md:flex flex-col items-center text-[14px] justify-center text-gray-600 hover:text-gray-900 hidden cursor-pointer"
                         onClick={() => {
+                            setShowSearch(false);
                             setShowPopup(false);
                             navigate("/network");
                         }}
@@ -166,7 +263,10 @@ function Navbar() {
 
                     {/* DP */}
                     <div
-                        onClick={() => setShowPopup((prev) => !prev)}
+                        onClick={() => {
+                            setShowSearch(false);
+                            setShowPopup((prev) => !prev);
+                        }}
                         className="h-[45px] w-[45px] rounded-full text-gray-500 cursor-pointer"
                     >
                         <img
